91c153534003eecc19622e88265dd5c25a4109e9
Andy Karpov <andy.karpov@gmail.com>
2016-06-28 01:46:28 +0300

    bugfixes for linecnt

--- src/video.v
+++ src/video.v
@@ -50,6 +50,12 @@ reg [8:0] old_count_v;
 wire sync_frame = (old_count_v == 9'd511) && (count_v == 9'd0);
 always @(posedge clk) begin
   h <= (hend || doubler_sync) ? 10'd0 : h + 10'd1;
+  if (hend || doubler_sync) begin
+  	if (wrlinecnt == 1) 
+		wrlinecnt <= 0;
+	else 
+		wrlinecnt <= 1;
+  end
   if(doubler_sync) v <= 0;
     else if (hend) v <= vend ? 10'd0 : v + 10'd1;
 
@@ -59,26 +65,15 @@ end
 wire [14:0] pixel_v = (!hpicture || !vpicture) ? 15'd0 : doubler_pixel;
 wire inpicture = hpicture && vpicture;
 
-wire [14:0] vga_pixel_v;
-wire [9:0] wraddr;
-wire [9:0] rdaddr;
-reg wrlinecnt = 1'b0;
-reg rdlinecnt = 1'b1;
+// ------------- converting from 512x480 to 640x480 --------------
 
-always @(posedge clk) begin
-	if (wrlinecnt == 1) 
-		wrlinecnt <= 0;
-	else 
-		wrlinecnt <= 1;
-end
+wire [14:0] vga_pixel_v; // color data readed from line buffer
+wire [9:0] wraddr;		 // write address to line buffer
+wire [9:0] rdaddr;		 // read address from line buffer
+reg wrlinecnt = 1'b0;	 // line number (0 or 1) for write
+reg rdlinecnt = 1'b1;	 // line number (0 or 1) for read
 
-always @(posedge clk_vga) begin
-	if (rdlinecnt == 1) 
-		rdlinecnt <= 0;
-	else 
-		rdlinecnt <= 1;
-end
-
+// line buffer instance (2 port ram with separate clocks for read / write)
 linebuf linebuf(
 	.wrclock(clk),
 	.wraddress(wraddr),
@@ -90,10 +85,11 @@ linebuf linebuf(
 	.q(vga_pixel_v)
 );
 
-wire vga_h_maxed = (vga_h==799);
-wire vga_v_maxed = (vga_v==523);
-wire vga_vend;
+wire vga_h_maxed = (vga_h==799); // horizontal limit 
+wire vga_v_maxed = (vga_v==523); // vertical limit
+wire vga_vend; // translated vend value to vga_clk clock domain
 
+// clock domain translation for vend
 crossdomain cx1(
 	.clkA(clk),
 	.clkB(clk_vga),
@@ -101,13 +97,20 @@ crossdomain cx1(
 	.outB(vga_vend)
 );
 
+// vga H counter
 always @(posedge clk_vga) begin
-    if(vga_h_maxed)
+    if(vga_h_maxed) begin
         vga_h <= 0;
+	    if (rdlinecnt == 1) 
+			rdlinecnt <= 0;
+		else 
+			rdlinecnt <= 1;
+	end
     else
         vga_h <= vga_h + 1;
 end
 
+// vga V counter
 always @(posedge clk_vga) begin
     if(vga_h_maxed)
     begin
@@ -118,26 +121,28 @@ always @(posedge clk_vga) begin
     end
 end
 
-assign wraddr = h + (wrlinecnt ? 512 : 0);
-assign rdaddr = (vga_inpicture) ? vga_h - 64 + (rdlinecnt ? 512 : 0) : 9'd0;
+assign wraddr = h + (wrlinecnt ? 512 : 0); // write address to linebuffer
+assign rdaddr = (vga_inpicture) ? vga_h - 64 + (rdlinecnt ? 512 : 0) : 9'd0; // read address from linebuffer
+
+// ------------- VGA output assignments ---------------
 
 wire  [4:0]   vga_r = (vga_inpicture) ? vga_pixel_v[4:0] : 5'd0;
 wire  [4:0]   vga_g = (vga_inpicture) ? vga_pixel_v[9:5] : 5'd0;
 wire  [4:0]   vga_b = (vga_inpicture) ? vga_pixel_v[14:10] : 5'd0;
 wire         sync_h = ((vga_h >= (640 + 16)) && (vga_h < (640 + 16 + 96)));
 wire         sync_v = ((vga_v >= (480 + 11))  && (vga_v < (480 + 11 + 2)));
-wire vga_hpicture  = (vga_h >= 64 && vga_h < 512+64);             // 512px in line
-wire vga_vpicture  = (vga_v >= 1 && vga_v < 479); // 480 lines
-wire vga_inpicture = vga_hpicture && vga_vpicture;
+wire   vga_hpicture = (vga_h >= 64 && vga_h < 512+64);             // 512px in line
+wire   vga_vpicture = (vga_v >= 1 && vga_v < 479); // 480 lines
+wire  vga_inpicture = vga_hpicture && vga_vpicture;
 
 assign       VGA_HS = ~sync_h;
 assign       VGA_VS = ~sync_v;
 
-assign       VGA_R = vga_r;
-assign       VGA_G = vga_g;
-assign       VGA_B = vga_b;
+assign       VGA_R 	= vga_r;
+assign       VGA_G 	= vga_g;
+assign       VGA_B 	= vga_b;
 
-assign       VGA_BLANK = (vga_h >= 640 || vga_v >= 480);
+assign       VGA_BLANK 	  = (vga_h >= 640 || vga_v >= 480);
 assign       VGA_HCOUNTER = vga_h;
 assign       VGA_VCOUNTER = vga_v;
 
