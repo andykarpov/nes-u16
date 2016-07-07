431a7df07894c14a88104fc789640937292c0695
Andy Karpov <andy.karpov@gmail.com>
2016-07-08 00:41:56 +0300

    hotfix, added new roms

--- rtl/video/fb_video.v
+++ rtl/video/fb_video.v
@@ -52,31 +52,29 @@ assign rdaddress = (hdmi_inpicture) ? ((h2 - 52) + (v2 * 256)) : 15'b0;
 reg [15:0] pallut[0:63];
 initial $readmemh("nes_palette.txt", pallut);
 wire [14:0] pixel_v = pallut[pixel_o][14:0];
-// no memory for pallette conversion :(
-
-//always @(posedge clk_vga) begin
-//	pixel_v <= {pixel_o[5:4], 3'b000, pixel_o[3:2], 3'b000, pixel_o[1:0], 3'b000};
-//end
-
-always @(posedge clk_vga) begin
-  if (h_maxed)
-  	h <= 0;
-  else
-  	h <= h + 1;
-end
 
 always @(posedge clk_vga) begin
   if (h_maxed) begin
- 	 if (v_maxed) 
- 	 	v <= 0;
- 	 else 
- 	 	v <= v + 1;
+  	h <= 0;
+  	h2 <= 0;
+  end
+  else begin
+  	h <= h + 1;
+  	h2 <= ((h + 1) >> 1);
   end;
 end
 
 always @(posedge clk_vga) begin
-	h2 <= (h >> 1); //h / 2; 
-	v2 <= (v >> 1); //v / 2;
+  if (h_maxed) begin
+ 	 if (v_maxed) begin
+ 	 	v <= 0;
+ 	 	v2 <= 0;
+ 	 end
+ 	 else begin
+ 	 	v <= v + 1;
+ 	 	v2 <= ((v + 1) >> 1);
+ 	 end;
+  end;
 end
 
 // ------------- hdmi output assignments ---------------
